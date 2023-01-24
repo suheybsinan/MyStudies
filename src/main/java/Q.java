9e25dc6dca6e9e540c32dcf51777fd554006b03c
Sinan <ssuheybsinann@gmail.com>
2023-01-25 00:05:13 +0300

    MyStudies

--- src/main/java/Q.java
+++
@@ -1,9 +0,0 @@
-public class Q {
-
-    public static void main(String[] args) {
-        StringBuilder str = new StringBuilder(7);
-        str.append("Java");
-        System.out.println(str.capacity() + "," + str.length());
-    }
-
-}
